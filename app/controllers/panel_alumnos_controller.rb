48f264bbda99c450661fdb7bfd4af93468557d3a
luis <luis@localhost.localdomain>
2019-04-03 17:22:26 -0700

    Se cambio la tabla en el panel de alumnos

--- app/controllers/panel_alumnos_controller.rb
+++ app/controllers/panel_alumnos_controller.rb
@@ -6,5 +6,7 @@ class PanelAlumnosController < ApplicationController
     @fr_inscripcion_registros = FrInscripcionRegistro.where("user_id = ?", current_user.id )
     @it_inscripcion_registros = ItInscripcionRegistro.where("user_id = ?", current_user.id )
     @examen_colocacion_idiomas = ExamenColocacionIdioma.where("user_id = ?", current_user.id )
+    @cuota_ipn = CuotaCurso.find_by(descripcion: "Cuota IPN").nombre
+    @cuota_externos = CuotaCurso.find_by(descripcion: "Cuota externos").nombre
   end
 end
