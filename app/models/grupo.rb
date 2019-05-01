eb40f077873778d8d915a851f54e178bc2e4f3e4
luis <luis@localhost.localdomain>
2019-04-30 18:13:32 -0700

    se reemplazo al jefe de educacion continua por el coordinador de celex

--- app/models/grupo.rb
+++ app/models/grupo.rb
@@ -43,8 +43,8 @@ class Grupo < ApplicationRecord
     "#{idioma} #{nivel.downcase}"
   end
 
-  def jefe_educacion_continua
-    rol = Role.find_by(nombre: "Jefe Educación Continua").id
+  def coordinador_celex
+    rol = Role.find_by(nombre: "Coordinación CELEX").id
     usuarios = User.find_by(role: rol).nombre_paterno_materno
   end
 
